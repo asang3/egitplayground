f0e934986b856f1b3dfea9590b019338e5523caa
Abhinav Sangal <abhinav.sangal777@gmail.com>
2016-05-28 12:37:46 +0530

    Adjust Examples output messages

--- com.sangal.git.test/src/com/sangal/git/test/TestRun.java
+++ com.sangal.git.test/src/com/sangal/git/test/TestRun.java
@@ -2,6 +2,7 @@ package com.sangal.git.test;
 
 public class TestRun {
 	public static void main(String[] args) {
-		System.out.println("git is fun");
+		System.out.println("git is cool");
+		System.out.println("THis is new");
 	}
 }
