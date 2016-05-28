9717f45752a8ba0d607116ca2c14ef2c1235f74a
Abhinav Sangal <abhinav.sangal777@gmail.com>
2016-05-28 13:12:45 +0530

    This is to test merger

--- com.sangal.git.test/src/com/sangal/git/test/TestRun.java
+++ com.sangal.git.test/src/com/sangal/git/test/TestRun.java
@@ -4,5 +4,6 @@ public class TestRun {
 	public static void main(String[] args) {
 		System.out.println("git is cool");
 		System.out.println("THis is new");
+		System.out.println("This is to test merget");
 	}
 }
